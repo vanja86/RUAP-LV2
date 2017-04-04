63ac7f303ba0bf9e44a263c8c4b31a4a72e87baf
fkulic <filip.kulic@outlook.com>
2017-04-04 13:48:47 +0200

    Ex3 zavrsen Task 2

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -18,6 +18,15 @@ namespace ContactManager.Controllers
             this.contactRepository = new ContactRepository();
         }
 
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+
         public Contact[] Get()
         {
             {
