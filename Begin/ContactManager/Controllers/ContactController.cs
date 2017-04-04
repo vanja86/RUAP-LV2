39ce9596f09d1aa3dc560aecbe59a844524e7295
fkulic <filip.kulic@outlook.com>
2017-04-04 13:07:50 +0200

    Zavrsen Task 3

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContactManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,13 +10,21 @@ namespace ContactManager.Controllers
 {
     public class ContactController : ApiController
     {
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
-	            {
-		            "Hello",
-		            "World"
-	            };
+            return new Contact[]
+    {
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        },
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        }
+    };
         }
     }
 }
